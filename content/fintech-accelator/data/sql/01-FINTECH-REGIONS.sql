26ec2b7c42dd075a17247a9f16d6bc20fb44f128
Doc-UP-AlejandroJaimes <juan.carrillo@unipamplona.edu.co>
2025-05-02 11:53:30 -0500

    feat(fintech-accelerator): add seed data for countries, regions, and payment methods - Added SQL files to populate FINTECH_REGIONS, FINTECH_COUNTRIES, and   FINTECH_PAYMENT_METHODS - Included FK reference values for countries"

---
+++ content/fintech-accelator/data/sql/01-FINTECH-REGIONS.sql
@@ -0,0 +1,12 @@
+-- INSERT RECORDS IN REGIONS
+INSERT INTO fintech.regions (name) VALUES
+('Africa'),
+('Asia'),
+('Australia and Oceania'),
+('Caribbean'),
+('Europe'),
+('Island Territories'),
+('Middle East'),
+('North America and Central America'),
+('South America');
+COMMIT;
---
+++ content/fintech-accelator/data/sql/03-FINTECH-PAYMENT-METHODS.sql
@@ -0,0 +1,7 @@
+-- INSERT RECORDS IN METHODS
+INSERT INTO fintech.payment_methods (name) VALUES
+('NFC'),
+('Chip'),
+('Código QR'),
+('Ingreso manual');
+COMMIT;
